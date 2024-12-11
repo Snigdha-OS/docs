2728ac5b6667c7064203079c4e155fe35994aa47
Eshan Roy <m.eshanized@gmail.com>
2024-12-12 01:24:44 +0530

    🚀 feat(_test): language update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			// title: 'DOCUMENTATION | SNIGDHA OS',
-			title: {
-				en: 'DOCUMENTATION | SNIGDHA OS',
-				'in-IN': 'प्रलेखन | स्निग्धा ओएस',
-				'bn-BN': 'ডকুমেন্টেশন | স্নিগ্ধা ওএস',
-			},
-			defaultLocale: 'root', // optional
-			locales: {
-				root: {
-				  label: 'English',
-				  lang: 'en', // lang is required for root locales
-				},
-				'in-in': {
-				  label: 'हिन्दी',
-				  lang: 'in-IN',
-				},
-				'bn-bn': {
-					label: 'বাংলা',
-					lang: 'bn-BN'
-				}
-			  },
-			social: {
-				github: 'https://github.com/Snigdha-OS/',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Overview', slug: '' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: {
+        en: 'DOCUMENTATION | SNIGDHA OS',
+        'hi-IN': 'प्रलेखन | स्निग्धा ओएस',
+        'bn-BD': 'ডকুমেন্টেশন | স্নিগ্ধা ওএস',
+      },
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English',
+          lang: 'en',
+        },
+        'hi-IN': {
+          label: 'हिन्दी',
+          lang: 'hi-IN',
+        },
+        'bn-BD': {
+          label: 'বাংলা',
+          lang: 'bn-BD',
+        },
+      },
+      social: {
+        github: 'https://github.com/Snigdha-OS/',
+      },
+      sidebar: [
+        // {
+        //   label: 'Introduction',
+        //   items: [
+        //     { label: 'Overview', slug: 'introduction/overview' },
+        //   ],
+        // },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
--- src/content/docs/introduction/overview-01.md
+++
@@ -1,7 +0,0 @@
----
-title: "Overview"
-description: "Introduction to the Snigdha OS documentation."
----
-# Overview
-
-Welcome to the overview of Snigdha OS. Here's what you need to know.
