32d6671eb465e7b0a2453b73e57b7d9fb53962b1
Eshan Roy <m.eshanized@gmail.com>
2024-12-12 00:29:55 +0530

    initial__

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,36 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			// title: 'DOCUMENTATION | SNIGDHA OS',
+			title: {
+				en: 'DOCUMENTATION | SNIGDHA OS',
+				'in-IN': 'प्रलेखन | स्निग्धा ओएस',
+				'bn-BN': 'ডকুমেন্টেশন | স্নিগ্ধা ওএস',
+			},
+			defaultLocale: 'root', // optional
+			locales: {
+				root: {
+				  label: 'English',
+				  lang: 'en', // lang is required for root locales
+				},
+				'in-in': {
+				  label: 'हिन्दी',
+				  lang: 'in-IN',
+				},
+				'bn-bn': {
+					label: 'বাংলা',
+					lang: 'bn-BN'
+				}
+			  },
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/Snigdha-OS/',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Introduction',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Overview', slug: '' },
 					],
 				},
 				{
---
+++ src/content/docs/introduction/overview-01.md
@@ -0,0 +1,7 @@
+---
+title: "Overview"
+description: "Introduction to the Snigdha OS documentation."
+---
+# Overview
+
+Welcome to the overview of Snigdha OS. Here's what you need to know.
